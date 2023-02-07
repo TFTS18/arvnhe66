e8f8f1ca697b8ac2d2ea755cb7241dbe6f8e30df
TFTS <funane18@mccc.jp>
2023-02-07 16:06:03 +0900

    02071606

--- cg/cg.ino
+++ cg/cg.ino
@@ -17,12 +17,13 @@ int hA2 = 300;
 int hA1 = 300;
 int hA3 = 300;
 int hA0 = 300;
-int flag = 0;
 void setup(){
 pinMode(motorA,OUTPUT); 
 pinMode(motorB,OUTPUT); 
 pinMode(motorC,OUTPUT); 
 pinMode(motorD,OUTPUT); 
+pinMode(PWM_motAB,OUTPUT); 
+pinMode(PWM_motCD,OUTPUT); 
 Serial.begin(9600);
 }
 
@@ -62,84 +63,61 @@ void loop() {
       digitalWrite(motorA,HIGH);
       digitalWrite(motorB,LOW);
       analogWrite(PWM_motCD,0); 
-      digitalWrite(motorC,LOW);
+      digitalWrite(motorC,HIGH);
       digitalWrite(motorD,LOW);
   }else if(vA0 < hA0 && vA1 >= hA1 && vA2 >= hA2 && vA3 >= hA3){
       //W B B B ┤右折
       analogWrite(PWM_motAB,235); 
       digitalWrite(motorA,HIGH);
       digitalWrite(motorB,LOW);
-      analogWrite(PWM_motCD,190); 
-      digitalWrite(motorC,LOW);
-      digitalWrite(motorD,HIGH);
-      delay(1000);
+      analogWrite(PWM_motCD,0); 
+      digitalWrite(motorC,HIGH);
+      digitalWrite(motorD,LOW);
+      delay(1600);
     }else if(vA0 < hA0 && vA1 < hA1 && vA2 >= hA2 && vA3 >= hA3){
       //W W B B ┤右折
       analogWrite(PWM_motAB,235); 
       digitalWrite(motorA,HIGH);
       digitalWrite(motorB,LOW);
-      analogWrite(PWM_motCD,190); 
-      digitalWrite(motorC,LOW);
-      digitalWrite(motorD,HIGH);
-      delay(1000);
+      analogWrite(PWM_motCD,0); 
+      digitalWrite(motorC,HIGH);
+      digitalWrite(motorD,LOW);
+      delay(1600);
   }else if(vA0 < hA0 && vA1 >= hA1 && vA2 < hA2 && vA3 < hA3){
       //w b w w 左折
       analogWrite(PWM_motAB,0);   
-      digitalWrite(motorA,LOW);
+      digitalWrite(motorA,HIGH);
       digitalWrite(motorB,LOW);
       analogWrite(PWM_motCD,190); 
       digitalWrite(motorC,HIGH);
       digitalWrite(motorD,LOW);
   }else if(vA0 >= hA0 && vA1 >= hA1 && vA2 >= hA2 && vA3 < hA3){
       //B B B W ┤左折
-      analogWrite(PWM_motAB,230);   
-      digitalWrite(motorA,LOW);
-      digitalWrite(motorB,HIGH);
+      analogWrite(PWM_motAB,0); 
+      digitalWrite(motorA,HIGH);
+      digitalWrite(motorB,LOW);
       analogWrite(PWM_motCD,190); 
       digitalWrite(motorC,HIGH);
       digitalWrite(motorD,LOW);
-      delay(1000);
+      delay(1600);
   }else if(vA0 >= hA0 && vA1 >= hA1 && vA2 < hA2 && vA3 < hA3){
       //B B W W ┤左折
-      analogWrite(PWM_motAB,230);   
-      digitalWrite(motorA,LOW);
-      digitalWrite(motorB,HIGH);
+      analogWrite(PWM_motAB,0); 
+      digitalWrite(motorA,HIGH);
+      digitalWrite(motorB,LOW);
       analogWrite(PWM_motCD,190); 
       digitalWrite(motorC,HIGH);
       digitalWrite(motorD,LOW);
-      delay(1000);
+      delay(1600);
   }else if(vA0 < hA0 && vA1 < hA1 && vA2 < hA2 && vA3 < hA3){
       //w w w w
       //現在調査中　適当なプログラムを入れている
-      analogWrite(PWM_motAB,230);   
-      digitalWrite(motorA,LOW);
-      digitalWrite(motorB,HIGH);
+      analogWrite(PWM_motAB,235); 
+      digitalWrite(motorA,HIGH);
+      digitalWrite(motorB,LOW);
       analogWrite(PWM_motCD,190); 
       digitalWrite(motorC,HIGH);
       digitalWrite(motorD,LOW);
-      delay(20);
-  }else if(vA0 >= hA0 && vA1 >= hA1 && vA2 >= hA2 && vA3 >= hA3){
-      //B B B B 謎
-        if(flag == 0){
-          analogWrite(PWM_motAB,0); 
-          digitalWrite(motorA,LOW);
-          digitalWrite(motorB,LOW);
-          analogWrite(PWM_motCD,0); 
-          digitalWrite(motorC,LOW);
-          digitalWrite(motorD,LOW);
-          delay(5000);
-          flag = 1;
-        }else if(flag == 1){
-          analogWrite(PWM_motAB,235); 
-          digitalWrite(motorA,LOW);
-          digitalWrite(motorB,HIGH);
-          analogWrite(PWM_motCD,190); 
-          digitalWrite(motorC,LOW);
-          digitalWrite(motorD,HIGH);
-          flag = 2;
-        }else{
-          flag = 0;
-        }
   }else{
       delay(1);
   }
