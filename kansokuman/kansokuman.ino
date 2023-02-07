6022a4fa9fb8daa5c3b9dcf3e4296e28736df02e
TFTS <funane18@mccc.jp>
2023-02-07 17:05:24 +0900

    02071705

--- kansokuman/kansokuman.ino
+++ kansokuman/kansokuman.ino
@@ -7,7 +7,7 @@ int vA1 = 0;
 int vA2 = 0;
 int vA3 = 0;
 const int motorA = 11;
-const int motorB = 0;
+const int motorB = 10;
 const int PWM_motAB = 6;
 const int motorC = 9;
 const int motorD = 8;
@@ -20,58 +20,17 @@ pinMode(motorA,OUTPUT);
 pinMode(motorB,OUTPUT); 
 pinMode(motorC,OUTPUT); 
 pinMode(motorD,OUTPUT); 
-pinMode(13,OUTPUT);
 pinMode(PWM_motAB,OUTPUT); 
 pinMode(PWM_motCD,OUTPUT); 
-pinMode(13,OUTPUT);
-pinMode(motorA,OUTPUT); 
-pinMode(motorB,OUTPUT); 
-pinMode(motorC,OUTPUT); 
-pinMode(motorD,OUTPUT); 
 Serial.begin(9600);
 }
 
 void loop() {
-  vA0 = analogRead(A0);
-  vA1 = analogRead(A1);
-  vA2 = analogRead(A2);
-  vA3 = analogRead(A3);
-  //モーター 右AB左CD
-  if (vA1 >= hA1 && vA2 >= hA2){
-    //左、右ともに白なら直進
-    analogWrite(PWM_motAB,0); 
-    analogWrite(PWM_motCD,0); 
-  }else if(vA1 < hA1 && vA2 >= hA2){
-    //左が黒、右が白ならに右へ旋回
-    analogWrite(PWM_motAB,0);   
-    analogWrite(PWM_motCD,0); 
-  }else if(vA1 >= hA1 && vA2 < hA2){
-    //左が白、右が黒ならに左へ旋回
-    analogWrite(PWM_motAB,00); 
-    analogWrite(PWM_motCD,0); 
-  }else if(vA1 < hA1 && vA2 < hA2){
-    analogWrite(PWM_motAB,0); 
-    analogWrite(PWM_motCD,0); 
-  }
-    delay(50);
-  if(TINPO == 6){
-    Serial.print("A0= "); 
-    Serial.print(vA0);
-    Serial.println();
-    Serial.print("A1== "); 
-    Serial.println(vA1);
-    Serial.print("A2= "); 
-    Serial.print(vA2);
-    Serial.println();
-    Serial.print("A3== "); 
-    Serial.println(vA3);
-    Serial.println();
-    TINPO = 0;
-  }else{
-    TINPO = TINPO + 1;
-  }
-    digitalWrite(motorA,HIGH);
-    digitalWrite(motorB,LOW);
-    digitalWrite(motorC,HIGH);
-    digitalWrite(motorD,LOW);
+     analogWrite(11,190); 
+     digitalWrite(10,HIGH);
+     digitalWrite(5,LOW);
+     analogWrite(9,235); 
+     digitalWrite(8,HIGH);
+     digitalWrite(6,LOW);
+     delay(1000);
 }
